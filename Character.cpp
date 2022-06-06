90bd134f0a7667580815b23848042d08e1de6b42
Armando Terrazas <mandotrs@icloud.com>
2022-06-06 11:47:55 -0500

    Modifiqué el archivo de Character.cpp

---
+++ Character.cpp
@@ -0,0 +1,22 @@
+#include "Character.h"
+
+Character::Character(){
+    this->name = "";
+}
+
+Character::Character(string name, Clothes* disguise){
+    this->name = name;
+    this->disguise = disguise;
+}
+
+string Character::getName(){
+    return this->name;
+}
+
+void Character::setName(string name){
+    this->name = name;
+}
+
+Clothes* Character::getDisguise() const{
+    return this->disguise;
+}
